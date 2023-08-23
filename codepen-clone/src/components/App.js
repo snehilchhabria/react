7d9ae36871f8bb394c0ace004c647a2f934cedce
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-23 22:47:05 +0530

    set up

--- codepen-clone/src/components/App.js
+++ codepen-clone/src/components/App.js
@@ -1,6 +1,19 @@
 function App() {
   return (
-    "Hello"
+    <>
+    <div classname="pane top-pane">
+      
+    </div>
+    <div classname="pane">
+    <iframe
+    title="output"
+    sandbox="allow-scripts"
+    frameBorder="0"
+    width="100%"
+    height="100%"
+     />
+    </div>
+    </>
   )
 }
 
