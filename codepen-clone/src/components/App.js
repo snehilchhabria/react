950ffeda899394edf62d95801abc185bc45a214c
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-23 22:50:39 +0530

    editor

--- codepen-clone/src/components/App.js
+++ codepen-clone/src/components/App.js
@@ -1,8 +1,13 @@
+import React from "react";
+import Editor from "./Editor";
+
 function App() {
   return (
     <>
     <div classname="pane top-pane">
-      
+      <Editor />
+      <Editor />
+      <Editor />
     </div>
     <div classname="pane">
     <iframe
