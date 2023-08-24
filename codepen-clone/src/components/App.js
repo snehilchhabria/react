07ff7e8751b1c6ea0420c759fbd2827dbeaeb86a
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-24 22:56:34 +0530

    timeout

--- codepen-clone/src/components/App.js
+++ codepen-clone/src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Editor from "./Editor";
 
 function App() {
@@ -6,7 +6,22 @@ function App() {
   const [html,setHtml] = useState('')
   const [css,setCss] = useState('')
   const [js,setJs] = useState('')
+  const [srcDoc, setSrcDoc] = useState('')
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc( `
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `
+      )
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  },[html,css,js])
   return (
     <>
     <div className="pane top-pane">
@@ -35,6 +50,7 @@ function App() {
     </div>
     <div className="pane">
     <iframe
+    srcDoc={srcDoc}
     title="output"
     sandbox="allow-scripts"
     frameBorder="0"
